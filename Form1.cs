4fdb75f0ae77dbcd1102b03aff6f959306740651
VacalatyVictoria1 <55949524+VacalatyVictoria1@users.noreply.github.com>
2020-05-14 22:43:28 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ using System.IO;
 
 namespace Labaratory_2b
 {
-        public partial class Form1 : Form
+         public partial class Form1 : Form
     {
         Circle round;
         Cylinder cylinder;
@@ -25,31 +25,31 @@ namespace Labaratory_2b
             {
                 if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
                     return;
-                string filename = openFileDialog1.FileName;              
+                string filename = openFileDialog1.FileName;
                 BinaryReader br = new BinaryReader(File.Open(filename, FileMode.Open));
                 RichTextBoxResult.Text += br.ReadString();
                 br.Close();
                 MessageBox.Show("File open");
             }
         }
-                
+
         private void button1_Click(object sender, EventArgs e)
         {
             round.Radius = Convert.ToDouble(text_radius.Text == "" ? "0" : text_radius.Text);
-            richTextBoxRound.Text = round.ToString();
+            richTextBoxCircle.Text = round.ToString();
 
             cylinder.Height = Convert.ToDouble(text_Height.Text == "" ? "0" : text_Height.Text);
             cylinder.Radius = Convert.ToDouble(text_radius.Text == "" ? "0" : text_radius.Text);
             richTextBoxCylinder.Text = cylinder.ToString();
 
-            dataGridView1.Rows.Add(text_radius.Text, text_Height.Text, round.Square(), 
+            dataGridView1.Rows.Add(text_radius.Text, text_Height.Text, round.Square(),
                 round.Length(), cylinder.Volume());
-             
+
         }
 
         private void StartCalculating_Click(object sender, EventArgs e)
         {
-            RichTextBoxResult.Text = GetResult();           
+            RichTextBoxResult.Text = GetResult();
         }
 
         public string Max_Square()
@@ -58,11 +58,11 @@ namespace Labaratory_2b
             int number = 0;
             for (int i = 0; i < dataGridView1.RowCount; i++)
             {
-                if (Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value) > max_square)                        
+                if (Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value) > max_square)
                 {
                     max_square = Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value);
                     number = i;
-                 }               
+                }
             }
             return "\nMax area of a circle = " + Convert.ToString(max_square);
         }
@@ -74,7 +74,7 @@ namespace Labaratory_2b
             {
                 average_volume += Convert.ToDouble(dataGridView1.Rows[i].Cells[4].Value);
             }
-            average_volume = average_volume / Convert.ToDouble((dataGridView1.RowCount- 1));
+            average_volume = average_volume / Convert.ToDouble((dataGridView1.RowCount - 1));
 
             return "\nAverage volume of a cylinder = " + Convert.ToString(average_volume);
         }
@@ -94,13 +94,13 @@ namespace Labaratory_2b
             if (result == DialogResult.Yes)
             {
                 if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
-                    return;                
+                    return;
                 string filename = saveFileDialog1.FileName;
                 BinaryWriter bw = new BinaryWriter(File.Open(filename, FileMode.OpenOrCreate));
                 bw.Write(RichTextBoxResult.Text.ToString());
                 bw.Close();
                 MessageBox.Show("File save!");
-            }            
+            }
         }
     }
 }
